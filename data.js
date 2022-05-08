5203dbcee39c0a1ce69944beee234e1db7e7b348
SLMuhil <slmuhil151@gmail.com>
2022-05-08 16:32:40 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,33 @@
+let movies = [
+    {
+        name: 'Ratchasan',
+        des: 'Arun gives up on his dream of becoming a filmmaker and takes up the job of a police officer after his father death. He then attempts to track down a psychotic killer who targets schoolgirls.',
+        image: 'images/rt.JPG'
+    },
+    {
+        name: 'Vikram Vedha',
+        des: 'Vikram, a pragmatic policeman, and his partner Simon are on the hunt to capture Vedha. When Vedha voluntarily surrenders, he offers to tell Vikram a story, throwing Vikram life into disarray.',
+        image: 'images/vv.JPG'
+    },
+    {
+        name: 'The Dark Knight',
+        des: 'After Gordon, Dent and Batman begin an assault on Gothams organised crime, the mobs hire the Joker, a psychopathic criminal mastermind who offers to kill Batman and bring the city to its knees',
+        image: 'images/sliders.JPG'
+    },
+    {
+        name: 'Avengers Infity War',
+        des: 'The Avengers must stop Thanos, an intergalactic warlord, from getting his hands on all the infinity stones. However, Thanos is prepared to go to any lengths to carry out his insane plan.',
+        image: 'images/slider6.JPEG'
+    },
+    {
+        name: 'Valimai',
+        des: 'Upcoming Film',
+        image: 'images/valimai.JPG'
+    },
+    {
+        name: 'Beast',
+        des: 'Upcoming Film',
+        image: 'images/beast.JPG'
+    },
+   
+]
